796da31c7ca44298b8cb81cf7df9692861275083
Vladimir Reshetnikov <v.reshetnikov@gmail.com>
2025-09-10 16:57:30 -0700

    Remove PipelineTransformsFunction test case
    Removed test for PipelineTransformsFunction in DefaultPassPipelineTests.

--- Vibe.Decompiler.Tests/Transformations/DefaultPassPipelineTests.cs
+++ Vibe.Decompiler.Tests/Transformations/DefaultPassPipelineTests.cs
@@ -22,18 +22,4 @@ public class DefaultPassPipelineTests
         // Verify that SimplifyRedundantAssignPass is included by checking its behavior
         Assert.IsType<IR.NopStmt>(fn.Blocks[0].Statements[0]);
     }
-
-    [Fact]
-    public void PipelineTransformsFunction()
-    {
-        var fn = new IR.FunctionIR("test");
-        var bb = new IR.BasicBlock(new IR.LabelSymbol("L0", 0));
-        bb.Statements.Add(new IR.AssignStmt(new IR.RegExpr("rax"), new IR.RegExpr("rax")));
-        fn.Blocks.Add(bb);
-
-        var pm = DefaultPassPipeline.Create();
-        pm.Run(fn);
-
-        Assert.IsType<IR.NopStmt>(fn.Blocks[0].Statements[0]);
-    }
 }
