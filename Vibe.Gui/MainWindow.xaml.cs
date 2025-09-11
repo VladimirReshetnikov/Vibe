8cfaa52c813f4a24e0d5009300ab4002ae36cb38
Vladimir Reshetnikov <v.reshetnikov@gmail.com>
2025-09-10 17:18:57 -0700

    Add icon to DLL tree view entries

--- Vibe.Gui/MainWindow.xaml.cs
+++ Vibe.Gui/MainWindow.xaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using Microsoft.Win32;
 using Vibe.Decompiler;
 
@@ -28,7 +29,13 @@ public partial class MainWindow : Window
             try
             {
                 var pe = new PEReaderLite(dlg.FileName);
-                var root = new TreeViewItem { Header = System.IO.Path.GetFileName(dlg.FileName), Tag = pe };
+
+                var icon = (ImageSource)FindResource("DllIcon");
+                var headerPanel = new StackPanel { Orientation = Orientation.Horizontal };
+                headerPanel.Children.Add(new Image { Source = icon, Width = 16, Height = 16, Margin = new Thickness(0, 0, 4, 0) });
+                headerPanel.Children.Add(new TextBlock { Text = System.IO.Path.GetFileName(dlg.FileName) });
+
+                var root = new TreeViewItem { Header = headerPanel, Tag = pe };
                 foreach (var name in pe.EnumerateExportNames().OrderBy(n => n))
                 {
                     root.Items.Add(new TreeViewItem { Header = name, Tag = new ExportItem { Pe = pe, Name = name } });
