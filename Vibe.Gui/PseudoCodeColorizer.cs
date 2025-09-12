088b1e45a96e2003212b5014ad43f50830dd683f
Vladimir Reshetnikov <v.reshetnikov@gmail.com>
2025-09-12 10:32:53 -0700

    Enhance C/C++ syntax highlighter

--- Vibe.Gui/PseudoCodeColorizer.cs
+++ Vibe.Gui/PseudoCodeColorizer.cs
@@ -24,19 +24,26 @@ internal sealed class PseudoCodeColorizer : DocumentColorizingTransformer
     private static readonly HashSet<string> Keywords = new(StringComparer.Ordinal)
     {
         // C keywords
-        "if","else","while","for","return","break","continue","int","float","double","char","void",
-        "auto","extern","register","restrict","volatile","unsigned","signed","long","short","sizeof",
-        "goto","switch","case","default","do","enum","static","const","struct","union","typedef",
-        "inline","_Alignas","_Alignof","_Atomic","_Bool","_Complex","_Generic","_Imaginary","_Noreturn",
-        "_Static_assert","_Thread_local","bool","true","false","null","class","public","private","protected"
+        "auto","break","case","char","const","continue","default","do","double","else","enum","extern","float","for","goto","if","inline","int","long","register","restrict","return","short","signed","sizeof","static","struct","switch","typedef","union","unsigned","void","volatile","while",
+        "_Alignas","_Alignof","_Atomic","_BitInt","_Bool","_Complex","_Decimal32","_Decimal64","_Decimal128","_Generic","_Imaginary","_Noreturn","_Static_assert","_Thread_local",
+
+        // C++ keywords
+        "alignas","alignof","and","and_eq","asm","bitand","bitor","bool","catch","char8_t","char16_t","char32_t","class","compl","concept","consteval","constexpr","constinit","const_cast","co_await","co_return","co_yield","decltype","delete","dynamic_cast","explicit","export","friend","mutable","namespace","new","noexcept","not","not_eq","nullptr","operator","or","or_eq","private","protected","public","reinterpret_cast","requires","static_assert","static_cast","template","this","thread_local","throw","true","try","typeid","typename","using","virtual","wchar_t","xor","xor_eq",
+
+        // Common aliases and additional keywords
+        "bool","false","true","class","struct","union","enum","template","typename","typedef","constexpr","constinit","consteval","concept","requires"
     };
 
     private static readonly HashSet<string> PreprocessorKeywords = new(StringComparer.Ordinal)
     {
-        "define","undef","include","if","ifdef","ifndef","else","elif","endif","error","pragma","line"
+        "define","undef","include","include_next","if","ifdef","ifndef","elif","else","endif","error","pragma","line","warning","import","region","endregion"
     };
 
     private bool _inMultiLineComment;
+    private bool _inString;
+    private bool _rawString;
+    private char _stringQuote;
+    private string? _rawStringDelimiter;
 
     protected override void ColorizeLine(DocumentLine line)
     {
@@ -62,6 +69,23 @@ internal sealed class PseudoCodeColorizer : DocumentColorizingTransformer
             }
         }
 
+        if (_inString)
+        {
+            int endString = ContinueString(text, 0);
+            if (endString >= 0)
+            {
+                ChangeLinePart(lineOffset, lineOffset + endString,
+                    part => part.TextRunProperties.SetForegroundBrush(StringBrush));
+                start = endString;
+            }
+            else
+            {
+                ChangeLinePart(lineOffset, line.EndOffset,
+                    part => part.TextRunProperties.SetForegroundBrush(StringBrush));
+                return;
+            }
+        }
+
         while (start < text.Length)
         {
             char c = text[start];
@@ -78,7 +102,7 @@ internal sealed class PseudoCodeColorizer : DocumentColorizingTransformer
                         part => part.TextRunProperties.SetForegroundBrush(PreprocessorBrush));
 
                     // Highlight included file names surrounded by <> or ""
-                    if (string.Equals(directive, "include", StringComparison.Ordinal))
+                    if (string.Equals(directive, "include", StringComparison.Ordinal) || string.Equals(directive, "include_next", StringComparison.Ordinal))
                     {
                         int j = i;
                         while (j < text.Length && char.IsWhiteSpace(text[j])) j++;
@@ -99,6 +123,7 @@ internal sealed class PseudoCodeColorizer : DocumentColorizingTransformer
                     continue;
                 }
             }
+
             if (c == '/' && start + 1 < text.Length)
             {
                 if (text[start + 1] == '/')
@@ -127,43 +152,24 @@ internal sealed class PseudoCodeColorizer : DocumentColorizingTransformer
                 }
             }
 
-            if (c == '"')
+            if (IsStringStart(text, start))
             {
-                int i = start + 1;
-                while (i < text.Length)
-                {
-                if (text[i] == '\\' && i + 1 < text.Length) i += 2;
-                    else if (text[i] == '"') { i++; break; }
-                    else i++;
-                }
-                ChangeLinePart(lineOffset + start, lineOffset + i,
-                    part => part.TextRunProperties.SetForegroundBrush(StringBrush));
-                start = i;
-                continue;
-            }
-
-            if (c == '\'')
-            {
-                int i = start + 1;
-                while (i < text.Length)
-                {
-                    if (text[i] == '\\' && i + 1 < text.Length) i += 2;
-                    else if (text[i] == '\'') { i++; break; }
-                    else i++;
-                }
-                ChangeLinePart(lineOffset + start, lineOffset + i,
-                    part => part.TextRunProperties.SetForegroundBrush(StringBrush));
-                start = i;
+                start = ScanString(text, start, lineOffset);
                 continue;
             }
 
             if (char.IsDigit(c))
             {
-                int i = start + 1;
-                if (c == '0' && i < text.Length && (text[i] == 'x' || text[i] == 'X'))
+                int i = start;
+                if (c == '0' && i + 1 < text.Length && (text[i + 1] == 'x' || text[i + 1] == 'X'))
                 {
-                    i++;
+                    i += 2;
                     while (i < text.Length && Uri.IsHexDigit(text[i])) i++;
+                }
+                else if (c == '0' && i + 1 < text.Length && (text[i + 1] == 'b' || text[i + 1] == 'B'))
+                {
+                    i += 2;
+                    while (i < text.Length && (text[i] == '0' || text[i] == '1')) i++;
                 }
                 else
                 {
@@ -172,6 +178,13 @@ internal sealed class PseudoCodeColorizer : DocumentColorizingTransformer
                     {
                         if (text[i] == '.') hasDot = true;
                         i++;
+                    }
+
+                    if (i < text.Length && (text[i] == 'e' || text[i] == 'E' || text[i] == 'p' || text[i] == 'P'))
+                    {
+                        i++;
+                        if (i < text.Length && (text[i] == '+' || text[i] == '-')) i++;
+                        while (i < text.Length && char.IsDigit(text[i])) i++;
                     }
                 }
                 while (i < text.Length && char.IsLetter(text[i])) i++; // numeric suffixes
@@ -198,4 +211,137 @@ internal sealed class PseudoCodeColorizer : DocumentColorizingTransformer
             start++;
         }
     }
+
+    private bool IsStringStart(string text, int index)
+    {
+        char c = text[index];
+        if (c == '\"' || c == '\'') return true;
+
+        int i = index;
+        if (c == 'u')
+        {
+            i++;
+            if (i < text.Length && text[i] == '8') i++;
+        }
+        else if (c == 'U' || c == 'L')
+        {
+            i++;
+        }
+        else if (c != 'R')
+        {
+            return false;
+        }
+
+        if (i < text.Length && text[i] == 'R')
+        {
+            i++;
+        }
+        return i < text.Length && (text[i] == '\"' || text[i] == '\'');
+    }
+
+    private int ScanString(string text, int start, int lineOffset)
+    {
+        int i = start;
+
+        // Parse prefixes
+        if (text[i] == 'u')
+        {
+            i++;
+            if (i < text.Length && text[i] == '8') i++;
+        }
+        else if (text[i] == 'U' || text[i] == 'L')
+        {
+            i++;
+        }
+
+        bool raw = false;
+        if (i < text.Length && text[i] == 'R')
+        {
+            raw = true;
+            i++;
+        }
+
+        if (i >= text.Length) return start + 1;
+
+        char quote = text[i];
+        if (quote != '\"' && quote != '\'') return start + 1;
+
+        if (raw && quote == '\"')
+        {
+            int delimStart = i + 1;
+            int parenIndex = text.IndexOf('(', delimStart);
+            string delimiter = parenIndex >= 0 ? text.Substring(delimStart, parenIndex - delimStart) : string.Empty;
+            string terminator = ")" + delimiter + "\"";
+            int end = parenIndex >= 0 ? text.IndexOf(terminator, parenIndex + 1, StringComparison.Ordinal) : -1;
+
+            if (end >= 0)
+            {
+                end += terminator.Length;
+                ChangeLinePart(lineOffset + start, lineOffset + end,
+                    part => part.TextRunProperties.SetForegroundBrush(StringBrush));
+                return end;
+            }
+            else
+            {
+                ChangeLinePart(lineOffset + start, line.EndOffset,
+                    part => part.TextRunProperties.SetForegroundBrush(StringBrush));
+                _inString = true;
+                _rawString = true;
+                _rawStringDelimiter = terminator;
+                return text.Length;
+            }
+        }
+        else
+        {
+            i++;
+            int end = i;
+            while (end < text.Length)
+            {
+                if (text[end] == '\\' && end + 1 < text.Length) end += 2;
+                else if (text[end] == quote)
+                {
+                    end++;
+                    ChangeLinePart(lineOffset + start, lineOffset + end,
+                        part => part.TextRunProperties.SetForegroundBrush(StringBrush));
+                    return end;
+                }
+                else end++;
+            }
+
+            ChangeLinePart(lineOffset + start, line.EndOffset,
+                part => part.TextRunProperties.SetForegroundBrush(StringBrush));
+            _inString = true;
+            _rawString = false;
+            _stringQuote = quote;
+            return text.Length;
+        }
+    }
+
+    private int ContinueString(string text, int start)
+    {
+        if (_rawString && _rawStringDelimiter != null)
+        {
+            int end = text.IndexOf(_rawStringDelimiter, start, StringComparison.Ordinal);
+            if (end >= 0)
+            {
+                _inString = false;
+                return end + _rawStringDelimiter.Length;
+            }
+            return -1;
+        }
+
+        int i = start;
+        while (i < text.Length)
+        {
+            if (text[i] == '\\' && i + 1 < text.Length) i += 2;
+            else if (text[i] == _stringQuote)
+            {
+                _inString = false;
+                return i + 1;
+            }
+            else i++;
+        }
+        return -1;
+    }
 }
+
