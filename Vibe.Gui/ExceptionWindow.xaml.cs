ec2be95b1c98298a60a99623192c638e6a0b1dac
Vladimir Reshetnikov <v.reshetnikov@gmail.com>
2025-09-12 07:04:43 -0700

    Enable copying exceptions via Ctrl+C

--- Vibe.Gui/ExceptionWindow.xaml.cs
+++ Vibe.Gui/ExceptionWindow.xaml.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Vibe.Gui;
 
@@ -9,5 +12,18 @@ public partial class ExceptionWindow : Window
     {
         InitializeComponent();
         DataContext = exceptions;
+    }
+
+    private void ExceptionsList_OnKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.C && (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+        {
+            var selectedItems = ExceptionsList.SelectedItems.Cast<string>();
+            if (selectedItems.Any())
+            {
+                Clipboard.SetText(string.Join(Environment.NewLine, selectedItems));
+                e.Handled = true;
+            }
+        }
     }
 }
